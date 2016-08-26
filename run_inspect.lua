e0ab1f12562344088139f9936d7a44a09239ed0e
Jinzheng Zhang <tianchaijz@gmail.com>
2016-08-26 12:58:31 +0800

    Add run_inspect.lua

---
+++ run_inspect.lua
@@ -0,0 +1,13 @@
+local linspect = require "linspect"
+local socket = require "socket"
+
+
+local function inspect(kind, ...)
+    local banner = string.rep("=", 20) .. " " .. kind
+    return linspect["inspect_" .. kind](...)
+end
+
+
+inspect("loadfile", "scripts/global.lua")
+inspect("loadfile", "scripts/local.lua")
+inspect("loadfile", "scripts/module.lua")
